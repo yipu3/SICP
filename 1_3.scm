75f78083500e8e81cdeeb272c4d25a2fe825abfe
Philip <5365604+breeze2501@users.noreply.github.com>
2023-01-13 23:42:09 +0800

    pretty

--- 1_3.scm
+++ 1_3.scm
@@ -1,7 +1,6 @@
 (define (square x) (* x x))
 
-(define (f x y z) (
-  - (+ (square x) (square y) (square z)) (square (min x y z))
-))
+(define (f x y z)
+  (- (+ (square x) (square y) (square z)) (square (min x y z))))
 
 (f 1 2 3)
